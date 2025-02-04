938b1a42a3aaba44c4e4a870930ed4833d881645
Rayan <rayan.ahmad97@gmail.com>
2025-02-04 11:49:48 +0100

    ny fordon class

--- Truck.java
+++ Truck.java
@@ -6,4 +6,10 @@ abstract public class Truck extends Vehicle {
     public Truck(int nrDoors, Color color, double enginePower, String modelName) {
         super(nrDoors, color, enginePower, modelName);
     }
+
+    abstract protected double rampOpen();
+    abstract protected double rampClose();
+    abstract protected double loadTruck();
+    abstract protected T unLoadTruck<T>;
+
 }
