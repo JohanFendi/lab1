e5cbc7048fda08829e68738c819cdb7ae16c63c0
Rayan <rayan.ahmad97@gmail.com>
2025-02-04 11:41:41 +0100

    ny fordon class

--- Car.java
+++ Car.java
@@ -6,4 +6,6 @@ abstract public class Car extends Vehicle {
     public Car(int nrDoors, Color color, double enginePower, String modelName) {
         super(nrDoors, color, enginePower, modelName);
     }
+
+
 }
---
+++ Truck.java
@@ -0,0 +1,9 @@
+
+import java.awt.*;
+
+abstract public class Truck extends Vehicle {
+
+    public Truck(int nrDoors, Color color, double enginePower, String modelName) {
+        super(nrDoors, color, enginePower, modelName);
+    }
+}
