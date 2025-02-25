f67a95de122974bbfc561af77ffb5c5d6b918ab7
JoosefL <joseflot@gmail.com>
2025-02-25 14:29:36 +0100

    update: från komposition till usage dependency

---
+++ src/model/Movement.java
@@ -0,0 +1,34 @@
+package src.model;
+
+public class Movement {
+
+    private Movement() {
+
+    }
+
+
+
+
+    public static void move(Position pos, Vector v, double speed){
+        pos.setX(pos.getX() + speed * v.getX());
+        pos.setY(pos.getY() + speed * v.getY());
+    }
+
+    public static void turnRight(Vector vector) {
+        double temp = vector.getY();
+        vector.setY(-vector.getX());
+        vector.setX(temp);
+    }
+
+    public static void turnLeft(Vector vector){
+        double temp = vector.getY();
+        vector.setY(vector.getX());
+        vector.setX(-temp);
+    }
+
+
+
+
+
+
+}
